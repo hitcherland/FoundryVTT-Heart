444d9eb38c3d25d8433dda2938d02775afd3163e
hitcherland <hitcherland@gmail.com>
2022-03-03 19:31:41 +0000

    Update version

--- foundryvtt.config.js
+++ foundryvtt.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     type: 'system',
     name: 'heart',
-    version: '0.4.0',
+    version: '0.4.1',
     author: 'hitcherland',
     title: 'Heart - The City Beneath',
     description: 'A red wet heaven slumbers fitful under the city of Spire. Purchase at https://rowanrookanddecard.com/product/heart-the-city-beneath-rpg/',
     minimumCoreVersion: '0.8.6',
-    compatibleCoreVersion: '9.249',
+    compatibleCoreVersion: '9.251',
     githubRepo: "hitcherland/foundryvtt-heart",
     githubBranch: "release",
 };
