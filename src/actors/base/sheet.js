d9eaea6cf2988cf5cb7133e4e28596570e4e9f74
Benjamin Cowley <3180645+Wigginns@users.noreply.github.com>
2023-03-23 17:56:53 -0600

    Consolidate actor sheet listeners to base (#7)
    * Add listeners to base actor sheet like items: DRY
    
    * Cleanup duplicated listeners and consolidate to actor/base
    
    * Remove delve changes from branch

--- src/actors/base/sheet.js
+++ src/actors/base/sheet.js
@@ -4,7 +4,7 @@ import HeartSheetMixin from '../../common/sheet';
 
 export default class HeartActorSheet extends HeartSheetMixin(ActorSheet) {
     static get type() { return 'base'; }
-    
+
     get template() {
         return sheetHTML.path;
     }
@@ -29,6 +29,107 @@ export default class HeartActorSheet extends HeartSheetMixin(ActorSheet) {
         data.system = this.actor.system;
 
         return data;
+    }
+
+    activateListeners(html) {
+        html.find('[data-action=add][data-type]').click(ev => {
+          const target = $(ev.currentTarget);
+          const type = target.data('type');
+          const itemData = target.data('data') || {};
+
+          const doc = new CONFIG.Item.documentClass({
+              type,
+              name: `New ${type}`,
+              system: itemData
+          });
+
+          this.actor.createEmbeddedDocuments('Item', [doc.toObject()]);
+        });
+
+        html.find('[data-action=view]').click(async ev => {
+          const uuid = $(ev.currentTarget).closest('[data-item-id]').data('itemId');
+          const item = await fromUuid(uuid);
+          item.sheet.render(true);
+        });
+
+        html.find('[data-action=delete]').click(async ev => {
+            const uuid = $(ev.currentTarget).closest('[data-item-id]').data('itemId');
+            const item = await fromUuid(uuid);
+            item.deleteDialog();
+        });
+
+        html.find('[data-action=item-roll]').click(async ev => {
+          const uuid = $(ev.currentTarget).closest('[data-item-id]').data('itemId');
+          const item = await fromUuid(uuid);
+          let rollOptions = {'stepIncrease': false, 'stepDecrease': false};
+
+          // TODO: Make step increase/decrease less of a mess?
+          if (ev.shiftKey) {
+            rollOptions.stepIncrease = true;
+          }
+          if (ev.altKey) {
+            rollOptions.stepDecrease = true;
+          }
+          if (ev.altKey && ev.shiftKey) {
+            rollOptions = {'stepIncrease': false, 'stepDecrease': false};
+          }
+
+          const roll = game.heart.rolls.ItemRoll.build({item}, {}, rollOptions);
+          await roll.evaluate({async: true});
+
+          roll.toMessage({
+              flavor: `${localizeHeart(item.name)} (<span class="item-type">${item.type}</span>)`,
+              speaker: {actor: this.actor.id}
+          });
+        });
+      
+        html.find('[data-action=roll]').click(async ev => {
+            const roll = await  game.heart.rolls.HeartRoll.build({
+                character: this.actor.id
+            });
+
+            roll.toMessage({
+                speaker: {actor: this.actor.id}
+            });
+        });
+
+        html.find('[data-action=stress-roll]').click(async ev => {
+            const roll = await game.heart.rolls.StressRoll.build({
+                character: this.actor.id
+            });
+
+            roll.toMessage({
+                speaker: {actor: this.actor.id}
+            });
+        });
+
+        html.find('[data-item-id] [data-action=activate]').click(async ev => {
+            const target = $(ev.currentTarget);
+            const uuid = target.closest('[data-item-id]').data('itemId');
+            const item = await fromUuid(uuid);
+            item.update({'system.active': true});
+        });
+
+        html.find('[data-item-id] [data-action=deactivate]').click(async ev => {
+            const target = $(ev.currentTarget);
+            const uuid = target.closest('[data-item-id]').data('itemId');
+            const item = await fromUuid(uuid);
+            item.update({'system.active': false});
+        });
+
+        html.find('[data-item-id] [data-action=complete]').click(async ev => {
+          const target = $(ev.currentTarget);
+          const uuid = target.closest('[data-item-id]').data('itemId');
+          const item = await fromUuid(uuid);
+          item.update({'system.complete': true});
+        });
+
+        html.find('[data-item-id] [data-action=uncomplete]').click(async ev => {
+            const target = $(ev.currentTarget);
+            const uuid = target.closest('[data-item-id]').data('itemId');
+            const item = await fromUuid(uuid);
+            item.update({'system.complete': false});
+        });
     }
 
     async _onDragStart(event) {
