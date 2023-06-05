507c51885fb26e31deca83768cb3e646701128f2
hitcherland <hitcherland@gmail.com>
2023-06-05 10:12:19 +0100

    Update version and add release notes for v0.8.1

--- foundryvtt.config.js
+++ foundryvtt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     type: 'system',
     name: 'heart',
-    version: '0.8.0',
+    version: '0.8.1',
     authors: ['hitcherland'],
     title: 'Heart - The City Beneath',
     description: 'A red wet heaven slumbers fitful under the city of Spire. Purchase at https://rowanrookanddecard.com/product/heart-the-city-beneath-rpg/',
