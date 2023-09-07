d4d646914a401bbd1e4baa09ce2b24bb8242ddc6
hitcherland <hitcherland@gmail.com>
2023-09-07 08:23:56 +0100

    Remove debugging line

--- src/index.js
+++ src/index.js
@@ -239,7 +239,6 @@ Hooks.once('ready', function () {
 
 
 Hooks.on('preCreateItem', function(document, data, options, userId) {
-    console.warn(document);
     document.updateSource({
         name: localizeHeart(document.name)
     });
