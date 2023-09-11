e80dc34405018d8138ee4f296969e9782b947bc4
hitcherland <hitcherland@gmail.com>
2023-09-12 00:02:39 +0100

    Remove unused file

--- dev-utils/build-css.js
+++
@@ -1,6 +0,0 @@
-// this might not be needed as we currently load this all
-// into one giant file. Might try not doing that and see how
-// the load times are
-const sass = require('sass');
-
-const result = sass.compile(scssFilename);
