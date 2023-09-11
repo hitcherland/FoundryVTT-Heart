736dad72643e081dd63eebe36c22af3c76b0bb19
hitcherland <hitcherland@gmail.com>
2023-09-11 23:14:42 +0100

    Update manifest target

--- dev-utils/build-manifest.js
+++ dev-utils/build-manifest.js
@@ -20,7 +20,7 @@ if (manifest.esmodules === undefined) manifest.esmodules = [`${config.id}.js`];
 const githubRepo = config.githubRepo;
 if (githubRepo) {
     if (manifest.url === undefined) manifest.url = `https://github.com/${githubRepo}`;
-    if (manifest.manifest === undefined) manifest.manifest = `https://raw.githubusercontent.com/${githubRepo}/${version}/${config.type}.json`;
+    if (manifest.manifest === undefined) manifest.manifest = `https://github.com/${githubRepo}/releases/latest/download/system.json`
     if (manifest.readme === undefined) manifest.readme = `https://raw.githubusercontent.com/${githubRepo}/${version}/README.md`;
     if (manifest.download === undefined) manifest.download = `https://github.com/${githubRepo}/releases/download/${version}/heart.zip`;
 }
