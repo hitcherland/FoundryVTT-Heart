5e425f1eff2151750023d22e8f708e8adab28f61
hitcherland <hitcherland@gmail.com>
2024-08-06 22:23:49 +0100

    Modify applications to better suit beat-tracker application

--- src/applications/base/application.js
+++ src/applications/base/application.js
@@ -1,50 +1,55 @@
 export default class HeartApplication extends FormApplication {
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            classes: ["form", "heart", this.formType],
-        });
-    }
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["form", "heart", this.formType],
+      resizable: true,
+    });
+  }
 
-    static get formType() {
-        return 'base'
-    }
-    
-    static async build(data, msg) {
-        try {
-            const valueData = Object.entries(data).reduce((map, [key, build]) => {
-                map[key] = build.value[0];
-                return map;
-            }, {});
+  static get formType() {
+    return "base";
+  }
 
-            await this._roll(valueData, msg);
-        } catch(err) {
-            return (new this({}, {
-                heart: data,
-                msg
-            })).render(true);
+  static async build(data, msg) {
+    try {
+      const valueData = Object.entries(data).reduce((map, [key, build]) => {
+        map[key] = build.value[0];
+        return map;
+      }, {});
+
+      await this._roll(valueData, msg);
+    } catch (err) {
+      return new this(
+        {},
+        {
+          heart: data,
+          msg,
         }
+      ).render(true);
     }
+  }
 
-    static async _roll(data) {
-        ui.notifications.error(`_roll has not been defined for ${this.formType}`);
+  static async _roll(data) {
+    ui.notifications.error(`_roll has not been defined for ${this.formType}`);
+  }
+
+  get title() {
+    if (this.constructor.formType !== "base") {
+      return game.i18n.localize(
+        `heart.applications.${this.constructor.formType}.title`
+      );
+    } else {
+      return super.title;
     }
-    
-    get title() {
-        if(this.constructor.formType !== 'base') {
-            return game.i18n.localize(`heart.applications.${this.constructor.formType}.title`);
-        } else {
-            return super.title
-        }
-    }
+  }
 
+  activateListeners(html) {
+    html.find("button").click((ev) => {
+      ev.preventDefault();
+    });
 
-    activateListeners(html) {
-        html.find('button').click(ev => {
-            ev.preventDefault();
-        });
-
-        html.find('[data-action=cancel]').click(ev => {
-            this.close();
-        });
-    }
+    html.find("[data-action=cancel]").click((ev) => {
+      this.close();
+    });
+  }
 }
--- src/applications/beat-tracker/application.js
+++ src/applications/beat-tracker/application.js
@@ -1,50 +1,50 @@
-import applicationHTML from './application.html'; 
+import applicationHTML from "./application.html";
+import HeartApplication from "../base/application.js";
 
-export default class BeatTrackerApplication extends FormApplication {
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            template: applicationHTML.path, 
-            classes: ["form", "heart", this.formType],
+export default class BeatTrackerApplication extends HeartApplication {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      template: applicationHTML.path,
+      classes: ["form", "heart", this.formType],
+      resizable: true,
+    });
+  }
+
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      template: applicationHTML.path,
+    });
+  }
+
+  static get formType() {
+    return "beat-tracker";
+  }
+
+  getData() {
+    const data = super.getData();
+
+    const charactersWithBeats = game.actors
+      .filter((actor) => actor.type === "character")
+      .reduce((arr, character) => {
+        arr.push({
+          character,
+          activeBeats: character.items.filter(
+            (item) => item.type === "beat" && item.system.active
+          ),
         });
-    }
+        return arr;
+      }, []);
 
-    static get formType() {
-        return 'beat-tracker';
-    }
-    
+    return foundry.utils.mergeObject(data, {
+      characters: charactersWithBeats,
+    });
+  }
 
-    getData() {
-        const data = super.getData();
-    
-        const charactersWithBeats = game.actors
-            .filter(actor => actor.type === 'character')
-            .reduce((arr, char) => {
-                const activeBeats = char.items.filter(item => 
-                    item.type === 'beat' && item.system.active
-                ).map(item => ({
-                    text: item.name,
-                    type: item.system.type
-                }));
-    
-                arr.push({
-                    id: char.id,
-                    name: char.name,
-                    activeBeats: activeBeats
-                });
-                return arr;
-            }, []);
-    
-        return foundry.utils.mergeObject(data, {
-            'characters': charactersWithBeats,
-        });
-    }
-    
+  activateListeners(html) {
+    super.activateListeners(html);
 
-    activateListeners(html) {
-        super.activateListeners(html); 
-        
-        html.find('[data-action=close]').click(() => {
-            this.close();
-        });
-    }
+    html.find("[data-action=close]").click(() => {
+      this.close();
+    });
+  }
 }
